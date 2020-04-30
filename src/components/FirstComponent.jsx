89bbde71d3c56bfaaab040e2620da63e1d9e893f
Rharuow <haryssonsoares@gmail.com>
2020-04-30 09:13:58 -0300

    arrow, Unamed and default functions

---
+++ src/components/FirstComponent.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function first() {
+    return <h1>Primeiro Component!</h1>
+}
+
+// export default function() {
+//     return <h1>Primeiro Component! (Unamed function)</h1>
+// }
+
+// export default () => <h1>Primeiro Component (Arrow function)</h1>
+
+
+export default first
--- src/index.js
+++ src/index.js
@@ -1,32 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import First from './components/FirstComponent'
 
 const el = document.getElementById('root')
 
 ReactDOM.render(
-    <ol>
-        <li>Harysson</li>
-        <li>Rafael</li>
-    </ol>
-    , el)
-
-// const root = document.getElementById('root')
-
-// let createOl = document.createElement('ol')
-// let createLi = document.createElement('li')
-// let text = document.createTextNode('Harysson')
-
-// createLi.appendChild(text)
-
-// createOl.appendChild(createLi)
-
-// root.appendChild(createOl)
-
-// createLi = document.createElement('li')
-// text = document.createTextNode('Rafael')
-
-// createLi.appendChild(text)
-
-// createOl.appendChild(createLi)
-
-// root.appendChild(createOl)
+    <div>
+        <First/>
+    </div>,
+el)
