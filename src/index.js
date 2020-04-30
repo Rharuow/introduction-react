8c407cc0347f45b26145421ced650f5f01e965b6
Rharuow <haryssonsoares@gmail.com>
2020-04-30 08:48:45 -0300

    native JS vs JSX

--- src/index.js
+++ src/index.js
@@ -3,4 +3,30 @@ import ReactDOM from 'react-dom'
 
 const el = document.getElementById('root')
 
-ReactDOM.render(<h1>Olá React</h1>, el)
+ReactDOM.render(
+    <ol>
+        <li>Harysson</li>
+        <li>Rafael</li>
+    </ol>
+    , el)
+
+// const root = document.getElementById('root')
+
+// let createOl = document.createElement('ol')
+// let createLi = document.createElement('li')
+// let text = document.createTextNode('Harysson')
+
+// createLi.appendChild(text)
+
+// createOl.appendChild(createLi)
+
+// root.appendChild(createOl)
+
+// createLi = document.createElement('li')
+// text = document.createTextNode('Rafael')
+
+// createLi.appendChild(text)
+
+// createOl.appendChild(createLi)
+
+// root.appendChild(createOl)
