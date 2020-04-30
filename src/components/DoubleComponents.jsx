9a570698e8a5cd3bb70c460fb8c4b63b397bdee8
Rharuow <haryssonsoares@gmail.com>
2020-04-30 09:32:14 -0300

    multiple component at one file jsx

---
+++ src/components/DoubleComponents.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const CompA = (props) => <h1>{props.value}</h1>
+
+const CompB = (props) => <h1>{props.value}</h1>
+
+export { CompA, CompB }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import First from './components/FirstComponent'
+// import First from './components/FirstComponent'
+import { CompA, CompB as B } from './components/DoubleComponents'
 
 const el = document.getElementById('root')
 
 ReactDOM.render(
     <div>
-        <First value="Primeiro Component (from props)"/>
+        <CompA value="Component A"/>
+
+        <B value="Component B"/>
     </div>,
 el)
