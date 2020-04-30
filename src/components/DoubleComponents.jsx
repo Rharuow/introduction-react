7d01c9cc9644eea5e4368b8f9e1e91415e00308e
Rharuow <haryssonsoares@gmail.com>
2020-04-30 09:50:31 -0300

    how do u export componenets

--- src/components/DoubleComponents.jsx
+++ src/components/DoubleComponents.jsx
@@ -4,4 +4,12 @@ const CompA = (props) => <h1>{props.value}</h1>
 
 const CompB = (props) => <h1>{props.value}</h1>
 
+// OBS: Is not possible export Unamed function
+
+// export const CompA = (props) => <h1>{props.value}</h1>
+
+// export const CompB = (props) => <h1>{props.value}</h1>
+
+export default CompA
+
 export { CompA, CompB }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 // import First from './components/FirstComponent'
-// import { CompA, CompB as B } from './components/DoubleComponents'
-import MultipleElements from './components/MultipleElements'
+// import A, { CompB as B } from './components/DoubleComponents'
+import MultiElements from './components/MultipleElements'
 
 const el = document.getElementById('root')
 
 ReactDOM.render(
     <div>
-        <MultipleElements/>
+        <MultiElements/>
     </div>,
 el)
