b02c53f7352ac80023e0e67b4983f2fa1fcfc291
Rharuow <haryssonsoares@gmail.com>
2020-05-02 13:00:13 -0300

    The old this problem js

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,9 +1,25 @@
 import React, { Component } from 'react'
 
 export default class Counter extends Component {
+
+    // solution 1
+    // constructor(props) {
+    //     super(props)
+    //     this.plus = this.plus.bind(this)
+    // }
+
+    // solution 2
+    // <button onClick={() => this.plus()}>Inc</button>
     
+    // solution 3
+    // plus = () => {
+    //     console.log(this)
+    //     // this.props.value++
+    // }
+
     plus () {
-        this.props.value++
+        console.log(this)
+        // this.props.value++
     }
 
     render() {
