87e5fd074b82f6d4330139c15531cfbff4e19939
Rharuow <haryssonsoares@gmail.com>
2020-05-03 11:11:21 -0300

    how to use state

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,33 +2,49 @@ import React, { Component } from 'react'
 
 export default class Counter extends Component {
 
-    // solution 1
-    // constructor(props) {
-    //     super(props)
-    //     this.plus = this.plus.bind(this)
-    // }
+    state = {
+        number: 0
+    }
 
-    // solution 2
-    // <button onClick={() => this.plus()}>Inc</button>
-    
-    // solution 3
-    // plus = () => {
-    //     console.log(this)
-    //     // this.props.value++
-    // }
+    sum = () => {
+        this.alterNumber(1)
+    }
 
-    plus = () => {
-        console.log(this)
-        // this.props.value++
+    sub = () => {
+        this.alterNumber(-1)
+    }
+
+    alterNumber = (value) => {
+        this.setState({
+            number: this.state.number + value
+        })
     }
 
     render() {
         return (
             <div>
-                <h1>Value: {this.props.value}</h1>
-                <button onClick={this.plus}>Inc</button>
-                <button>Dec</button>
+                <h1>Value: {this.state.number}</h1>
+                <button onClick={this.sum}>Inc</button>
+                <button onClick={this.sub}>Dec</button>
+                <button onClick={() => this.alterNumber(10)}>Inc 10</button>
+                <button onClick={() => this.alterNumber(-10)}>Dec 10</button>
             </div>
         )
     }
 }
+
+
+// solution 1
+// constructor(props) {
+//     super(props)
+//     this.plus = this.plus.bind(this)
+// }
+
+// solution 2
+// <button onClick={() => this.plus()}>Inc</button>
+
+// solution 3
+// plus = () => {
+//     console.log(this)
+//     // this.props.value++
+// }
